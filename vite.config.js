4ecb7cdecf5a61dc76ff8692b7243153e3155ec3
moeezabbas <moeezabbas83@gmail.com>
2025-11-08 16:09:00 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   }
 })
